00fdb0053daefc6b1467e574f705a0bb69e43201
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-07-15 02:31:49 -0400

    bump

--- nix/ashwin-nixpkgs/default.nix
+++ nix/ashwin-nixpkgs/default.nix
@@ -1,6 +1,8 @@
 self : super:
 {
-    st = super.st.override {
-        src = builtins.fetchGit { url = "https://github.com/niwhsa9/st"; };
-    };
+    st = super.st.overrideAttrs ( old : rec {
+         src =( builtins.fetchGit { url = "https://github.com/niwhsa9/st"; });
+    });
+    
+    
 }  
