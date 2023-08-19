17519da01a33890a19925ce0a8d94c2254e959fc
Ashwin Gupta <ashwingupta2000@gmail.com>
2023-08-19 19:22:28 -0400

    machine specific hostname

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -30,7 +30,6 @@ in
   boot.loader.grub.devices = ["nodev"];
 
 
-  networking.hostName = "ashwin-desktop"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
